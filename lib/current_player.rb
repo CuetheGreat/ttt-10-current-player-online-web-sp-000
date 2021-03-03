af7869612ec4628b801cf4fd54df85289aeae7f9
Clayon Stewart <c.stewart89@gmail.com>
2021-03-03 17:36:13 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,17 @@
 def turn_count(board)
   counter = 0
-  board.each{ | item | counter += 1}
+  board.each{ | item |
+    if space_ocupied?(item)
+      counter += 1
+    end
+  }
   return counter
 end
 
-  
+def space_ocupied?(item)
+  if (item =="X" || item == "O")
+    return TRUE
+  else 
+    return FALSE
+  end
+end
